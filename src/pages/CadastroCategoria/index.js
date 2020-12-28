2a079844ee3b3a962f45092e4a9d872850f6b052
ramonis567 <ramonmatosferreira@gmail.com>
2020-12-28 12:51:00 -0300

    Novo commit

--- src/pages/CadastroCategoria/index.js
+++ src/pages/CadastroCategoria/index.js
@@ -54,7 +54,7 @@ export default function CadastroCategoria() {
     fetch(URL_TOP).then(async (serverReturn) => {
       const data = await serverReturn.json();
       setCategorias([
-        ...data, 
+        ...data,   
       ]);
     });
   }, []);
