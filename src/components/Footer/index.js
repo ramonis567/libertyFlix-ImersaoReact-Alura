7df80b8de9d4febfdd0c21ccca30d0cfc657ebfe
ramonis567 <ramonmatosferreira@gmail.com>
2020-12-28 13:02:15 -0300

    fim aula 04

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,8 +4,13 @@ import { FooterBase } from './styles';
 function Footer() {
   return (
     <FooterBase>
-      <a href="https://raw.githubusercontent.com/ramonis567/libertyFlix-ImersaoReact-Alura/main/src/assets/img/Logo2.svg" target="_blank" rel="noreferrer">
-        <img src="../../assets/img/Logo2.svg" alt="Logo 2HR Tech"/>
+      <a href="https://2hr.tech" target="_blank" rel="noreferrer">
+        <img 
+          src="https://raw.githubusercontent.com/ramonis567/libertyFlix-ImersaoReact-Alura/main/src/assets/img/Logo2.svg" 
+          width="100px" 
+          height="60px" 
+          alt="Logo 2HR Tech"
+        />
       </a>
       <p>
         Editar
